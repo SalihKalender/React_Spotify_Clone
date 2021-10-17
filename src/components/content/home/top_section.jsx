78300009b71b0982c717b677de39f23c64dff9f9
SalihKalender <m.salihk72@gmail.com>
2021-10-17 14:27:17 +0300

    top section'daki scroll event'ide yapildi, simdi global playlsitler

--- src/components/content/home/top_section.jsx
+++ src/components/content/home/top_section.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
+import { useEffect, useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import { faSortDown } from '@fortawesome/free-solid-svg-icons'
 import ProfileSrc from '../../../images/profile_picture.png'
 
+
+
 const top_section = () => {
+    const section_target = useRef(null)
+    const handleScroll = function(event) {
+        const scroll = window.scrollY
+        let opacity = 0
+        if(scroll < 46) {
+            opacity = scroll / 45
+        } 
+        else {
+            opacity = 1
+        }
+        if(section_target.current.style.backgroundColor != null) {
+            section_target.current.style.backgroundColor = `rgba(0,0,0, ${opacity})` 
+        }
+        console.log(section_target.current.style.backgroundColor)  
+    }
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+    }, [])
     return (
-        <div className="top_section">
+        <div className="top_section" ref={section_target}>
             <div className="buttons">
                 <div className="button_container">
                     <FontAwesomeIcon icon={faChevronLeft} />
