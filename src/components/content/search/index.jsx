974e554f42d6db726c8de00b0b47d3f649718b46
SalihKalender <m.salihk72@gmail.com>
2021-10-18 21:35:04 +0300

    search box izgarasi yapildi

--- src/components/content/search/index.jsx
+++ src/components/content/search/index.jsx
@@ -1,11 +1,116 @@
 import React from 'react'
 import TopSection from '../home/top_section'
+import '../../../styles/search/style.scss'
 export const index = (props) => {
     const is_Search = props.location.pathname
     return (
         <div className="homepage_container">
             <div className="page_container">
                 <TopSection is_search={is_Search} />
+                <div className="search_section_container">
+                    <h2 className="header">Hepsine göz at</h2>
+                    <div className="playlist_boxes">
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                        <div className="box">
+                            <div className="content"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
