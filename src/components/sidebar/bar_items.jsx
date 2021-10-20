9527d42573726ec57cdf77de324e3cc0b6888148
SalihKalender <m.salihk72@gmail.com>
2021-10-20 15:41:55 +0300

    menu active class'lar duzenlendi

--- src/components/sidebar/bar_items.jsx
+++ src/components/sidebar/bar_items.jsx
@@ -6,32 +6,26 @@ import { faSwatchbook } from '@fortawesome/free-solid-svg-icons'
 import { NavLink } from 'react-router-dom'
 const bar_items = () => {
     return (
-        <ul className="items_list">
-            <li className="active">
-                <NavLink activeClassName="active" to="/">
+        <div className="items_list">
+                <NavLink activeClassName="active" exact to="/">
                     <FontAwesomeIcon icon={faHome} size="lg"/>
                     <span className="item_text">
                         Home Page
                     </span>
                 </NavLink>   
-            </li>
-            <li>
                 <NavLink activeClassName="active" to="/search">
                     <FontAwesomeIcon icon={faSearch} size="lg" />
                     <span className="item_text">
                         Search
                     </span>
                 </NavLink>
-            </li>
-            <li>
                <NavLink activeClassName="active" to="/library">
                 <FontAwesomeIcon icon={faSwatchbook} size="lg"/>
                     <span className="item_text">
                         Your Library
                     </span>
                </NavLink>
-            </li>
-        </ul>
+        </div>
     )
 }
 
